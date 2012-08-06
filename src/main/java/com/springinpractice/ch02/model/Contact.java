ec201a4817eea89f5ea4690ec2b82cc3ff282f81
Willie Wheeler <willie.wheeler@gmail.com>
2012-08-05 20:32:14 -0700

    Adding ORM

--- src/main/java/com/springinpractice/ch02/model/Contact.java
+++ src/main/java/com/springinpractice/ch02/model/Contact.java
@@ -1,10 +1,24 @@
 package com.springinpractice.ch02.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 import com.springinpractice.util.StringUtils;
 
 /**
  * @author Willie Wheeler (willie.wheeler@gmail.com)
  */
+@Entity
+@Table(name = "contact")
+@NamedQuery(
+	name = "findContactsByEmail",
+	query = "from Contact where email = :email")
 public class Contact {
 	private Long id;
 	private String lastName;
@@ -12,34 +26,42 @@ public class Contact {
 	private String middleInitial;
 	private String email;
 	
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	public Long getId() { return id; }
 	
 	public void setId(Long id) { this.id = id; }
 	
+	@Column(name = "last_name")
 	public String getLastName() { return lastName; }
 	
 	public void setLastName(String lastName) {
 		this.lastName = StringUtils.cleanup(lastName);
 	}
 	
+	@Column(name = "first_name")
 	public String getFirstName() { return firstName; }
 	
 	public void setFirstName(String firstName) {
 		this.firstName = StringUtils.cleanup(firstName);
 	}
 	
+	@Column(name = "mi")
 	public String getMiddleInitial() { return middleInitial; }
 	
 	public void setMiddleInitial(String mi) {
 		this.middleInitial = StringUtils.cleanup(mi);
 	}
 	
+	@Column
 	public String getEmail() { return email; }
 	
 	public void setEmail(String email) {
 		this.email = StringUtils.cleanup(email);
 	}
 	
+	@Transient
 	public String getFullName() {
 		String fullName = lastName + ", " + firstName;
 		if (! (middleInitial == null || "".equals(middleInitial.trim()))) {
--- src/main/java/com/springinpractice/ch02/service/impl/ContactServiceImpl.java
+++ src/main/java/com/springinpractice/ch02/service/impl/ContactServiceImpl.java
@@ -2,17 +2,14 @@ package com.springinpractice.ch02.service.impl;
 
 import static org.springframework.util.Assert.notNull;
 
-import java.util.HashMap;
 import java.util.List;
 
 import javax.inject.Inject;
 
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.springinpractice.ch02.model.Contact;
 import com.springinpractice.ch02.service.ContactService;
@@ -24,74 +21,53 @@ import com.springinpractice.ch02.service.ContactService;
  * @author Willie Wheeler (willie.wheeler@gmail.com)
  */
 @Service
+@Transactional
 public class ContactServiceImpl implements ContactService {
-	private static final String CREATE_SQL =
-			"insert into contact (last_name, first_name, mi, email) values (:lastName, :firstName, :mi, :email)";
-	private static final String FIND_ALL_SQL =
-			"select id, last_name, first_name, mi, email from contact";
-	private static final String FIND_ALL_BY_EMAIL_LIKE_SQL =
-			"select id, last_name, first_name, mi, email from contact where email like :email";
-	private static final String FIND_ONE_SQL =
-			"select id, last_name, first_name, mi, email from contact where id = :id";
-	private static final String UPDATE_SQL =
-			"update contact set last_name = :lastName, first_name = :firstName, mi = :mi, email = :email " +
-			"where id = :id";
-	private static final String DELETE_SQL =
-			"delete from contact where id = :id";
-	
-	@Inject private NamedParameterJdbcOperations jdbcTemplate;
-	@Inject private ContactRowMapper contactRowMapper;
+	@Inject private SessionFactory sessionFactory;
 
 	@Override
 	public void createContact(Contact contact) {
 		notNull(contact, "contact can't be null");
-		SqlParameterSource params = new MapSqlParameterSource()
-			.addValue("lastName", contact.getLastName())
-			.addValue("firstName", contact.getFirstName())
-			.addValue("mi", contact.getMiddleInitial())
-			.addValue("email", contact.getEmail());
-		KeyHolder keyHolder = new GeneratedKeyHolder();
-		jdbcTemplate.update(CREATE_SQL, params, keyHolder);
-		contact.setId(keyHolder.getKey().longValue());
+		getSession().save(contact);
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public List<Contact> getContacts() {
-		return jdbcTemplate.query(FIND_ALL_SQL, new HashMap<String, Object>(), contactRowMapper);
+		return getSession()
+			.createQuery("from Contact")
+			.list();
 	}
 	
 	@Override
+	@SuppressWarnings("unchecked")
 	public List<Contact> getContactsByEmail(String email) {
 		notNull(email, "email can't be null");
-		SqlParameterSource params = new MapSqlParameterSource("email", "%" + email + "%");
-		return jdbcTemplate.query(FIND_ALL_BY_EMAIL_LIKE_SQL, params, contactRowMapper);
+		return getSession()
+			.getNamedQuery("findContactsByEmail")
+			.setString("email", email)
+			.list();
 	}
 
 	@Override
 	public Contact getContact(Long id) {
 		notNull(id, "id can't be null");
-		SqlParameterSource params = new MapSqlParameterSource()
-			.addValue("id", id);
-		return jdbcTemplate.queryForObject(FIND_ONE_SQL, params, contactRowMapper);
+		return (Contact) getSession().get(Contact.class, id);
 	}
 
 	@Override
 	public void updateContact(Contact contact) {
 		notNull(contact, "contact can't be null");
-		SqlParameterSource params = new MapSqlParameterSource()
-			.addValue("id", contact.getId())
-			.addValue("lastName", contact.getLastName())
-			.addValue("firstName", contact.getFirstName())
-			.addValue("mi", contact.getMiddleInitial())
-			.addValue("email", contact.getEmail());
-		jdbcTemplate.update(UPDATE_SQL, params);
+		getSession().update(contact);
 	}
 
 	@Override
 	public void deleteContact(Long id) {
 		notNull(id, "id can't be null");
-		SqlParameterSource params = new MapSqlParameterSource()
-			.addValue("id", id);
-		jdbcTemplate.update(DELETE_SQL, params);
+		getSession().delete(getContact(id));
+	}
+	
+	private Session getSession() {
+		return sessionFactory.getCurrentSession();
 	}
 }
